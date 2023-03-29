c5eea8ab3cdf5c5734c2952458fa20b859bba7b6
Pascal Gouasmi <pgouasmi@gmail.com>
2023-03-29 18:33:57 +0200

    maj list_creator

--- push_swap_resources/push_swap_tools.c
+++ push_swap_resources/push_swap_tools.c
@@ -6,17 +6,17 @@
 /*   By: pgouasmi <pgouasmi@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 17:50:20 by pgouasmi          #+#    #+#             */
-/*   Updated: 2023/03/29 17:51:29 by pgouasmi         ###   ########.fr       */
+/*   Updated: 2023/03/29 18:33:02 by pgouasmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	print_values(t_list *a_head)
+void	print_values(t_list *head)
 {
 	t_list	*temp;
 
-	temp = a_head;
+	temp = head;
 	while (temp)
 	{
 		ft_printf("%d\n", temp->content);
