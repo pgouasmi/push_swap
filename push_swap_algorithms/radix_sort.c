7dc64c39a9d1e30efebe9b15ee85a5e54d53a4bf
Pascal Gouasmi <pgouasmi@gmail.com>
2023-04-03 18:55:30 +0200

    still no good but fixed previous changes

--- push_swap_algorithms/radix_sort.c
+++ push_swap_algorithms/radix_sort.c
@@ -6,7 +6,7 @@
 /*   By: pgouasmi <pgouasmi@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 19:37:42 by pgouasmi          #+#    #+#             */
-/*   Updated: 2023/04/03 17:39:39 by pgouasmi         ###   ########.fr       */
+/*   Updated: 2023/04/03 18:52:55 by pgouasmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,9 @@ void	ft_lst_radix_sort(t_list **a_head, t_list **b_head)
 	int elements_count;
 	t_list *temp;
 	int i;
-	int j;
 	int count;
 
 	mask = 1;
-	temp = *a_head;
 	count = 0;
 
 	/*verification de tous les bits d'un int 64bits*/
@@ -31,24 +29,26 @@ void	ft_lst_radix_sort(t_list **a_head, t_list **b_head)
 		temp = *a_head;
 		elements_count = ft_lstsize(temp); //compteur de tous les elements
 		i = 0;
-		j = 0;
 
-	/*compte le nbr d'elem a envoyer dans B*/
+		/*compte le nbr d'elem a envoyer dans B*/
 		while (temp)
 		{
 			if ((temp->content & mask) == 0)
 				count++;
 			temp = temp->next;
 		}
-	/*Detecte un element concordant bitwise et le fait remonter, pour le push B
-	Le compteur i permet de donner la place de temp (valeur que l'on teste)*/
+		/*Detecte un element concordant bitwise et le fait remonter, pour le push B
+		Le compteur i permet de donner la place de temp (valeur que l'on teste)*/
 		i = 0;
 		temp = *a_head;
 		while (temp)
 		{
 			/*condition voulue : si le premier element est KO bitwise mais le second OK --> PA*/
 			if ((i == 0) && (temp->next != NULL) && ((temp->content & mask) == 1) && ((temp->next->content & mask) == 0))
+			{
 				ft_swap_a(a_head);
+				temp = *a_head;
+			}
 			if ((temp->content & mask) == 0)
 			{
 				while (*a_head != temp)
@@ -58,31 +58,25 @@ void	ft_lst_radix_sort(t_list **a_head, t_list **b_head)
 					else
 						ft_reverse_rotate_a(a_head);
 				}
-				ft_push_b(a_head, b_head);
-				temp = *a_head;
+				if (count > 1)
+				{
+					ft_push_b(a_head, b_head);
+				}
 			}
 			temp = temp->next;
 			i++;
 		}
-
-		/*Envoie le dernier element de B a sa tete puis PA*/
-		if (count > 1)
+		temp = *b_head;
+		while (temp)
 		{
+			ft_push_a(a_head, b_head);
 			temp = *b_head;
-			while (temp)
-			{
-				ft_push_a(a_head, b_head);
-				temp = *b_head;
-			}
 		}
-		
 		if (ft_list_is_ordered(a_head))
-			return ;
+				return ;
 
 		/*On decale le bit de 1 vers la gauche, prenant la valeur 1*/
 		mask = mask << 1;
 	}
 	return ;
 }
-
-/*ajouter compteur d'elem a envoyer dans b. si count == 1, alors ne pas l'envoyer*/
